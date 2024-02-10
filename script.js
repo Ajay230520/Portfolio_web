fe2081c05bf8c4b18b40d80429d15a7c97ef3f1b
Ajay230520 <112386996+Ajay230520@users.noreply.github.com>
2024-02-10 23:42:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,8 +33,4 @@ $(document).ready(function(){
   });
 });
 
-const menuBtn = document.querySelector('.menu-btn');
 
-menuBtn.addEventListener('click',function(){
-  menuBtn.classList.toggle('nav-open')
-});
